b2a4f1e6ba1cfa2f29df21e7162693a3ceff45c0
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-02-05 13:56:15 +0100

    Install nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Express!</h1>');
+  res.send('<h1>Hello Express JS!</h1>');
 });
 
 app.get('/about', (req, res) => {
