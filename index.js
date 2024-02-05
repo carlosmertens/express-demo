8637df9feb60b51f2fd87ef56ffa89f0036a7428
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-02-05 13:53:17 +0100

    Create server and some url routes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('init...');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Express!</h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<p>About Page!</p>');
+});
+
+app.get('/api/skills', (req, res) => {
+  res.send(['js', 'ts', 'nodejs']);
+});
+
+app.listen(3000, () => console.log('Server ready on port 3000...'));
